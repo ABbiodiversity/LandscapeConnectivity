50ad3998a92aa6e6b99a0145e7c74c6fcadb80ee
beallen <ballen@ualberta.ca>
2022-10-03 10:16:58 -0600

    Conversion to geodatabase format

--- src/data-cleaning_functions.R
+++ src/data-cleaning_functions.R
@@ -1,11 +1,410 @@
 #
 # Title: Data cleaning functions
 # Created: November 24th, 2020
-# Last Updated: August 17th, 2022
+# Last Updated: September 16th, 2022
 # Author: Brandon Allen
 # Objectives: Functions required for cleaning the layers required for landscape connectivity
-# Keywords: Landscape Extraction, Multi-class Landscape Extraction
+# Keywords: Landscape cleaning, Cost assign, Cost distance
 #
+
+######################
+# Landscape cleaning # 
+######################~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+landscape_cleaning <- function(landcover.layer, boundary.layer, HUC.scale, HUC.id, HFI.year, arcpy) {
+  
+  ################## Question: Should we buffer the human footprint layer to remove artifacts?
+  # DECISION POINT # Implementation: We perform a 5m and -5m buffer to remove artifacts that would impact patch delineation
+  ################## Final Decision: We will not buffer the polygons.
+  
+  ##########################
+  # Create the geodatabase #
+  ##########################
+  
+  # Create geodatabase
+  arcpy$CreateFileGDB_management(out_folder_path = paste0(getwd(), "/data/processed/huc-8/", HFI.year, "/gis/"), 
+                                 out_name = paste0(HUC.id, ".gdb"))
+  
+  # Define workspace
+  arcpy$env$workspace <- paste0(getwd(), "/data/processed/huc-8/", HFI.year, "/gis/", HUC.id, ".gdb")
+  
+  ############################
+  # Select Analysis boundary #
+  ############################
+  
+  arcpy$Select_analysis(in_features = boundary.layer, 
+                        out_feature_class = "boundary", 
+                        where_clause =  paste0('"HUC_', HUC.scale,'" = ', "'", paste(HUC.id), "'"))
+  
+  ######################### 
+  # Clip Landcover to HUC # 
+  #########################
+  
+  # Extract landcover backfill layer
+  arcpy$PairwiseClip_analysis(in_features = landcover.layer, 
+                              clip_features = "boundary", 
+                              out_feature_class = "landcover")
+  
+  ############################
+  # Simplify landcover layer #
+  ############################
+  
+  # Add new fields to store simplified landcover
+  arcpy$AddField_management("landcover", "Current", "TEXT")
+  arcpy$AddField_management("landcover", "Reference", "TEXT")
+  
+  # Replace "-" in landcover names
+  arcpy$CalculateField_management(in_table = "landcover",
+                                  field = "Combined_ChgByCWCS",
+                                  expression = "!Combined_ChgByCWCS!.replace('-', '')",
+                                  expression_type = "PYTHON")
+  
+  # Create the replace function for landcovers
+  replace_function <- "
+def Reclass(arg):
+    if arg is 'Alkali':
+        return 'Grassland'
+    if arg is 'AlpineLarch':
+        return 'PineSpruce'
+    if arg is 'Bare':
+        return 'Other'
+    if arg is 'Conif':
+        return 'PineSpruce'
+    if arg is 'Decid':
+        return 'MixDecid'
+    if arg is 'Fir':
+        return 'PineSpruce'
+    if arg is 'GraminoidFen':
+        return 'LowlandForest'
+    if arg is 'GrassHerb':
+        return 'Grassland'
+    if arg is 'Marsh':
+        return 'LowlandForest'
+    if arg is 'Mixedwood':
+        return 'MixDecid'
+    if arg is 'Pine':
+        return 'PineSpruce'
+    if arg is 'Shrub':
+        return 'Grassland'
+    if arg is 'ShrubbyBog':
+        return 'LowlandForest'
+    if arg is 'ShrubbyFen':
+        return 'LowlandForest'
+    if arg is 'ShrubbySwamp':
+        return 'LowlandForest'
+    if arg is 'SnowIce':
+        return 'Water'
+    if arg is 'Spruce':
+        return 'PineSpruce'
+    if arg is 'TreedBogBSpr':
+        return 'LowlandForest'
+    if arg is 'TreedFenBSpr':
+        return 'LowlandForest'
+    if arg is 'TreedFenDecid':
+        return 'LowlandForest'
+    if arg is 'TreedFenLarch':
+        return 'LowlandForest'
+    if arg is 'TreedFenMixedwood':
+        return 'LowlandForest'
+    if arg is 'TreedSwampConif':
+        return 'LowlandForest'
+    if arg is 'TreedSwampDecid':
+        return 'LowlandForest'
+    if arg is 'TreedSwampFir':
+        return 'LowlandForest'
+    if arg is 'TreedSwampMixedwood':
+        return 'LowlandForest'
+    if arg is 'TreedSwampSpruce':
+        return 'LowlandForest'
+    if arg is 'TreedSwampForest':
+        return 'LowlandForest'
+    if arg is 'TreedWetlandMixedwood':
+        return 'LowlandForest'
+    if arg is 'Water':
+        return 'Water'"
+  
+  # Create simplified reference condition
+  arcpy$CalculateField_management(in_table = "landcover",
+                                  field = "Reference",
+                                  expression = "Reclass(!Combined_ChgByCWCS!)",
+                                  expression_type = "PYTHON", 
+                                  code_block = replace_function)
+  
+  # Copy FEATURE_TY to a new column
+  arcpy$CalculateField_management(in_table = "landcover",
+                                  field = "Current",
+                                  expression = "!FEATURE_TY!",
+                                  expression_type = "PYTHON")
+  
+  # Create UpdateCursor to replace blank footprint with reference & add ages to HARVEST-AREA
+  cursor <- arcpy$da$UpdateCursor(in_table = "landcover", 
+                                 field_names = list("Current", "YEAR", "Reference"),
+                                 where_clause = "\"Current\" IN ('', 'HARVEST-AREA')")
+  
+  # Update each row in the cursor
+  row <- iter_next(cursor)
+  while (!is.null(row)) {
+    
+    # If no footprint is present, replace with native cover
+    if(row[[1]] == "") {
+      
+      # Update the value
+      row[[1]] <- row[[3]]
+      cursor$updateRow(row)
+
+    } 
+    
+    if(row[[1]] == "HARVEST-AREA") {
+      
+      # Harvest areas with resistance (0-20 years)
+      if(row[[2]] == 0) {row[[1]] <- "HARVEST-AREA-15"} # Unknown ages are assumed old
+      if(HFI.year - row[[2]] >= 15) {row[[1]] <- "HARVEST-AREA-15"} # Old harvest areas
+      if(HFI.year - row[[2]] < 15 & HFI.year - row[[2]] >= 4) {row[[1]] <- "HARVEST-AREA-4-15"} # Medium harvest areas
+      if(HFI.year - row[[2]] < 4) {row[[1]] <- "HARVEST-AREA-4"} # Young harvest areas
+      
+      # Harvest areas with partial recovery (>20 years old)
+      if(row[[1]] == "HARVEST-AREA-15" & (HFI.year - row[[2]]) >= 20 & (HFI.year - row[[2]]) < 40 & row[[3]] %in% c("PineSpruce", "MixDecid")) {row[[1]] <- paste0(row[[3]], 20)} # 20-40
+      if(row[[1]] == "HARVEST-AREA-15" & (HFI.year - row[[2]]) >= 40 & (HFI.year - row[[2]]) < 60 & row[[3]] %in% c("PineSpruce", "MixDecid")) {row[[1]] <- paste0(row[[3]], 40)} # 40-60
+      if(row[[1]] == "HARVEST-AREA-15" & (HFI.year - row[[2]]) >= 60 & (HFI.year - row[[2]]) < 80 & row[[3]] %in% c("PineSpruce", "MixDecid")) {row[[1]] <- paste0(row[[3]], 60)} # 60-80
+      if(row[[1]] == "HARVEST-AREA-15" & (HFI.year - row[[2]]) >= 80 & row[[3]] %in% c("PineSpruce", "MixDecid")) {row[[1]] <- row[[3]]} # Full recovery
+      
+      # Update the value
+      cursor$updateRow(row)
+      
+    }
+    
+    # Go to the next row
+    row <- iter_next(cursor)  
+    
+  }
+  
+  # Clear row and cursor
+  rm(row, cursor)
+
+  # # Repair any stray geometry
+  # arcpy$RepairGeometry_management(in_features = "landcover",
+  #                                 delete_null = "KEEP_NULL")
+  
+  # Dissolve landcover into simplified current and reference landcovers.
+  arcpy$PairwiseDissolve_analysis(in_features = "landcover", 
+                                  out_feature_class = "current_landcover", 
+                                  dissolve_field = "Current", 
+                                  multi_part = "SINGLE_PART")
+  
+  arcpy$PairwiseDissolve_analysis(in_features = "landcover", 
+                                  out_feature_class = "reference_landcover", 
+                                  dissolve_field = "Reference", 
+                                  multi_part = "SINGLE_PART")
+  
+  # Remove the original landcover layer as it is no longer needed
+  arcpy$Delete_management("landcover")
+  
+}
+
+###############
+# Cost assign # 
+###############~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+cost_assign <- function(HUC.scale, HUC.id, HFI.year, barrier.lookup, arcpy) {
+  
+  # Define the workspace
+  arcpy$env$workspace <- paste0(getwd(), "/data/processed/huc-", HUC.scale, "/", HFI.year, "/gis/", HUC.id, ".gdb")
+  
+  ########################
+  # Assign barrier costs # 
+  ########################
+  
+  # Create the new fields
+  for (layer in c("reference_landcover", "current_landcover")) {
+    
+    # Create the new fields
+    arcpy$AddField_management(layer, "UFCost", "DOUBLE")
+    arcpy$AddField_management(layer, "LFCost", "DOUBLE")
+    arcpy$AddField_management(layer, "GCost", "DOUBLE")
+    arcpy$AddField_management(layer, "UFSource", "DOUBLE")
+    arcpy$AddField_management(layer, "LFSource", "DOUBLE")
+    arcpy$AddField_management(layer, "GSource", "DOUBLE")
+    
+    # Assign costs and sources
+    habitat.avail <- c(0, 0, 0) # Use for tracking if landcover is present in area
+    
+    # Use an update cursor to loop through the cells
+    landcover.name <- ifelse(layer == "reference_landcover", "Reference", "Current")
+    cursor <- arcpy$da$UpdateCursor(in_table = layer, 
+                                    field_names = list(landcover.name, "UFCost", "LFCost", "GCost", 
+                                                       "UFSource", "LFSource", "GSource"))
+    
+    # Update each row in the cursor
+    row <- iter_next(cursor)
+    while (!is.null(row)) {
+      
+      # Source assignment
+      # Assuming no source unless native cover types
+      row[[5]] <- 1
+      row[[6]] <- 1
+      row[[7]] <- 1
+      
+      if (row[[1]] %in% c("PineSpruce", "PineSpruce20", "PineSpruce40", "PineSpruce60",
+                          "MixDecid", "MixDecid20", "MixDecid40", "MixDecid60")){
+        
+        row[[5]] <- 0
+        habitat.avail[1] <- habitat.avail[1] - 1
+        
+      }
+      
+      if (row[[1]] == "LowlandForest"){
+        
+        row[[6]] <- 0
+        habitat.avail[2] <- habitat.avail[2] - 1
+        
+      }
+      
+      if (row[[1]] == "Grassland"){
+        
+        row[[7]] <- 0
+        habitat.avail[3] <- habitat.avail[3] - 1
+        
+      }
+      
+      # Cost assignment
+      
+      row[[2]] <- barrier.lookup$UplandCost[barrier.lookup$FEATURE_TY_ABMI == row[[1]]][1] # Only take the first value
+      row[[3]] <- barrier.lookup$LowlandCost[barrier.lookup$FEATURE_TY_ABMI == row[[1]]][1]
+      row[[4]] <- barrier.lookup$GrasslandCost[barrier.lookup$FEATURE_TY_ABMI == row[[1]]][1]
+
+      # Update the value
+      cursor$updateRow(row)
+      
+      # Go to the next row
+      row <- iter_next(cursor)  
+      
+    }
+  
+    # Clear row and cursor
+    rm(row, cursor)
+    
+  }
+  
+}
+
+###############
+# Cost assign # 
+###############~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+cost_distance <- function(status, HUC.scale, HUC.id, HFI.year, move.results, arcpy) {
+  
+  # Require packages
+  require(sf)
+  require(raster)
+  
+  # Define the workspace
+  arcpy$env$workspace <- paste0(getwd(), "/data/processed/huc-", HUC.scale, "/", HFI.year, "/gis/", HUC.id, ".gdb")
+  arcpy$Statistics_analysis(in_table = paste0(status, "_landcover"), 
+                            out_table = paste0(status, "_classes"), 
+                            statistics_fields = list(c("UFSource", "SUM"),
+                                                     c("LFSource", "SUM"),
+                                                     c("GSource", "SUM")))
+  
+  # Determine which landcover types are present (minimum of 2 patches)
+  habitat.avail <- read_sf(dsn = arcpy$env$workspace,
+                           layer = paste0(status, "_classes"))
+  habitat.avail <- habitat.avail$FREQUENCY - habitat.avail
+  habitat.avail <- colnames(habitat.avail)[habitat.avail > 2] 
+  habitat.avail <- gsub("SUM_", "",  habitat.avail)
+  
+  # Delete the table
+  arcpy$Delete_management(in_data = paste0(status, "_classes"))
+  
+  ###########################
+  # Calculate cost distance #
+  ###########################
+  
+  for (habitat.type in habitat.avail) {
+    
+    ########################
+    # Processes Cost Layer #
+    ########################
+    
+    # Define cost and source columns
+    cost.value <- gsub("Source", "Cost", habitat.type)
+    source.value <- habitat.type
+    
+    ################## Question: Should their be a movement cost when moving within patches?
+    # DECISION POINT # Implementation: We only implement movement penalties between patches, not within.
+    ################## Final Decision: No cost within patches.
+    
+    ################## Question: What is the cell size for our cost raster?
+    # DECISION POINT # Implementation: 10m cells.
+    ################## Final Decision: 10m cells.
+    
+    # Create cost raster (10m)
+    arcpy$PolygonToRaster_conversion(in_features = paste0(status, "_landcover"), 
+                                     value_field = cost.value, 
+                                     out_rasterdataset = paste0("costraster"), 
+                                     cell_assignment = "MAXIMUM_COMBINED_AREA", 
+                                     priority_field = cost.value, 
+                                     cellsize = 10)
+    
+    # Create source raster (10m)
+    arcpy$PolygonToRaster_conversion(in_features = paste0(status, "_landcover"), 
+                                     value_field = source.value, 
+                                     out_rasterdataset = "sourceclass", 
+                                     cell_assignment = "MAXIMUM_COMBINED_AREA", 
+                                     priority_field = source.value, 
+                                     cellsize = 10)
+    
+    # Create source raster (Native has value of 0)
+    arcpy$Reclassify_3d(in_raster = "sourceclass",
+                        reclass_field = "Value",
+                        remap = "0 1",
+                        out_raster = "sourceraster",
+                        missing_values = "NODATA")
+    
+    # Create the Euclidean distance raster
+    arcpy$sa$EucDistance(in_source_data = "sourceraster")
+    
+    # Create cumulative cost raster
+    arcpy$sa$CostDistance(in_source_data = "sourceraster", 
+                          in_cost_raster = "costraster")
+    
+    # Divide the two rasters to get the average multiplier cost for moving 
+    arcpy$Divide_3d(in_raster_or_constant1 = "CostDis_sour1", 
+                    in_raster_or_constant2 = "EucDist_sour1", 
+                    out_raster = paste0(getwd(), "/data/processed/huc-", HUC.scale, "/", HFI.year, "/scratch/movecost.tif"))
+    
+    # Store results
+    cost.weight <- raster(paste0(getwd(), "/data/processed/huc-", HUC.scale, "/", HFI.year, "/scratch/movecost.tif"))
+    cost.weight <- values(cost.weight)[!is.na(values(cost.weight))] # Remove NA
+    cost.weight <- cost.weight[cost.weight >= 1]
+    
+    # Define names
+    state.id <- ifelse(status == "current", "Cur", "Ref")
+    if(habitat.type == "UFSource") {col.id <- paste0("Up", state.id)}
+    if(habitat.type == "LFSource") {col.id <- paste0("Low", state.id)}
+    if(habitat.type == "GSource") {col.id <- paste0("Gr", state.id)}
+    
+    move.results[move.results$HUC_8 == HUC.id, col.id] <- ifelse(is.na(mean(cost.weight)) == TRUE, 1, mean(cost.weight))
+
+    # Remove all layers
+    arcpy$Delete_management(in_data = "costraster")
+    arcpy$Delete_management(in_data = "sourceclass")
+    arcpy$Delete_management(in_data = "sourceraster")
+    arcpy$Delete_management(in_data = "CostDis_sour1")
+    arcpy$Delete_management(in_data = "EucDist_sour1")
+    arcpy$Delete_management(in_data = paste0(getwd(), "/data/processed/huc-", HUC.scale, "/", HFI.year, "/scratch/movecost.tif"))
+    rm(cost.weight)
+    
+  }
+  
+  # Return the cost results
+  return(move.results)
+  
+}
+
+#######
+# OLD #
+########
+
 
 ########################
 # Landscape Extraction # Not updated for ArcPro
